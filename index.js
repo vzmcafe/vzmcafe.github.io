207e94618b57dd8eac65df0f8638731549c739fa
Vidoushee05 <47088287+Vidoushee05@users.noreply.github.com>
2019-03-08 18:18:03 +0000

    Included some javascript

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-<script>
 // When the button is clicked, pick a random recipe and show that recipe in alert box
 // Later on when we put the different recipes in different classes I'll change it to choose a random recipe class and go to that class
-var recipes = ["recipe1", "recipe2", "recipe3", "recipe4"];
-
-$(".random-generator").on(`click`(function(){
-  alert(recipes[Math.floor(Math.random()*recipes.length)]);
+$(".random-generator").click(function() {
+  var recipes = ["recipe1", "recipe2", "recipe3", "recipe4"];
+  var random = recipes[Math.floor(Math.random()*recipes.length)];
+  alert(random);
 });
-
-</script>
