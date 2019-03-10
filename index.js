a1bad1487a8229b6d5afcedb0c5dc849c5cea606
Vidoushee05 <47088287+Vidoushee05@users.noreply.github.com>
2019-03-10 20:10:39 +0000

    Changed positioning

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $('.random-generator').on('click', function() {
   var recipes = ["recipe1", "recipe2", "recipe3", "recipe4"];
   var random = recipes[Math.floor(Math.random()*recipes.length)];
-  alert(random)
+  show('#random')
 });
 
 $('.nav-link dropdown-toggle').on('click', function () {
