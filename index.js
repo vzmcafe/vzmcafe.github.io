7994c5a000a86ffb687047c764c431e8e850bf90
Vidoushee05 <47088287+Vidoushee05@users.noreply.github.com>
2019-03-10 19:11:06 +0000

    Fullpage code

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ $('.random-generator').on('click', function() {
 $('.nav-link dropdown-toggle').on('click', function () {
   $('.dropdown-menu').dropdown('toggle')
 });
+
+
+        new fullpage('#fullpage', {
+            anchors: ['page1', 'page2', 'page3', 'page4'],
+            navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
+            css3: true,
+            scrollingSpeed: 1000,
+            navigation: true,
+            slidesNavigation: true,
+            responsiveHeight: 330,
+            dragAndMove: true,
+            dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
+            controlArrows: false
+        });
